a5075e9edd533b07127258184d90d53938efa0a8
Wes Hendley <wrhendley@gmail.com>
2023-05-25 13:03:36 -0500

    Add my_each and my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,12 @@
 module Enumerable
   # Your code goes here
+
+  def my_all?
+    self.each do |value|
+      return false unless yield (value)
+    end
+    true
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +15,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for element in self
+      yield element
+    end
+  end
 end
